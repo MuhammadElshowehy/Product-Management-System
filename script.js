56e0869459ebbffd1a0a0e996853a7e696086648
muhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2023-04-12 15:36:44 +0200

    update & search products

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ let discount = document.getElementById("discount");
 let total = document.getElementById("total");
 let count = document.getElementById("count");
 let category = document.getElementById("category");
-let create = document.getElementById("create");
+let submit = document.getElementById("submit");
 let deleteAll = document.getElementById("delete-all");
+let search = document.getElementById("search");
+let mode = "create"; // mode: create or update product.
+let num; // fake var, his value will send from update function To create & update function.
 
 // 1- get total:
 function getTotal() {
@@ -25,7 +28,7 @@ taxes.addEventListener("keyup", getTotal);
 ads.addEventListener("keyup", getTotal);
 discount.addEventListener("keyup", getTotal);
 
-// 2- create products:
+// check local storage when program starting:
 let allProducts;
 if (localStorage.getItem("allProducts") != null) {
   allProducts = JSON.parse(localStorage.getItem("allProducts"));
@@ -33,25 +36,49 @@ if (localStorage.getItem("allProducts") != null) {
 } else {
   allProducts = [];
 }
-// create one product:
-create.addEventListener("click", function () {
+// 2- create & update products:
+submit.addEventListener("click", function () {
   let product = {
-    productTitle: productTitle.value,
+    productTitle: productTitle.value.toLowerCase(),
     productPrice: productPrice.value,
-    taxes: taxes.value,
-    ads: ads.value,
-    discount: discount.value,
+    taxes: taxes.value || 0,
+    ads: ads.value || 0,
+    discount: discount.value || 0,
     total: total.innerHTML,
-    count: count.value,
-    category: category.value,
+    count: count.value || 1,
+    category: category.value.toLowerCase(),
   };
-  allProducts.push(product);
+  if (
+    productTitle.value != "" &&
+    productPrice.value != "" &&
+    category.value != "" &&
+    count.value <= 100
+  ) {
+    /* start control mode */
+    if (mode === "create") {
+      // count logic:
+      if (product.count > 1) {
+        for (let i = 0; i < product.count; i++) {
+          allProducts.unshift(product);
+        }
+      } else {
+        // product.count = 1;
+        allProducts.unshift(product);
+      }
+    } else {
+      allProducts[num] = product;
+      count.style.display = "block";
+      submit.innerHTML = "create";
+      mode = "create";
+    }
+    /* end control mode */
+    clearInputs();
+  }
   localStorage.setItem("allProducts", JSON.stringify(allProducts));
-  clearInputs();
   showData();
 });
 
-// 3- clear inputs after click create button:
+// 3- clear inputs after click submit button:
 function clearInputs() {
   productTitle.value = "";
   productPrice.value = "";
@@ -62,24 +89,23 @@ function clearInputs() {
   count.value = "";
   category.value = "";
 }
-// 4- show data:
-function showData() {
-  // toggle show for delete all button:
-  function toggleDeleteAll() {
-    if (allProducts.length > 0) {
-      deleteAll.style.display = "block";
-    } else {
-      deleteAll.style.display = "none";
-    }
-  }
-  toggleDeleteAll();
 
+/* 4- start show data: */
+function toggleDeleteAll() {
+  if (allProducts.length > 0) {
+    deleteAll.style.display = "block";
+    deleteAll.innerHTML = `Delete All (${allProducts.length})`;
+  } else {
+    deleteAll.style.display = "none";
+  }
+}
+function showData() {
   if (allProducts.length == 0) {
     document.getElementById("tbody").innerHTML = "";
   } else {
-    let tbody = "";
+    let table = "";
     for (let i = 0; i < allProducts.length; i++) {
-      tbody += `
+      table += `
       <tr>
         <td>${i + 1}</td>
         <td>${allProducts[i].productTitle}</td>
@@ -90,14 +116,17 @@ function showData() {
         <td>${allProducts[i].total}</td>
         <td>${allProducts[i].count}</td>
         <td>${allProducts[i].category}</td>
-        <td><button id="update">update</button></td>
+        <td><button onclick="updateProduct(${i})" id="update">update</button></td>
         <td><button onclick="deleteProduct(${i})" id="delete">delete</button></td>
       </tr>
     `;
-      document.getElementById("tbody").innerHTML = tbody;
+      document.getElementById("tbody").innerHTML = table;
     }
   }
+  getTotal(); // to toggle style for total box
+  toggleDeleteAll();
 }
+/* end show data */
 
 // 5- delete one product:
 function deleteProduct(i) {
@@ -106,7 +135,7 @@ function deleteProduct(i) {
   showData();
 }
 
-// 6- delete all:
+// 6- delete all products:
 function deleteAllProducts() {
   allProducts = [];
   localStorage.removeItem("allProducts");
@@ -114,5 +143,79 @@ function deleteAllProducts() {
 }
 deleteAll.addEventListener("click", deleteAllProducts);
 
-// 7- count:
+// 7- update:
+function updateProduct(i) {
+  productTitle.value = allProducts[i].productTitle;
+  productPrice.value = allProducts[i].productPrice;
+  taxes.value = allProducts[i].taxes;
+  ads.value = allProducts[i].ads;
+  discount.value = allProducts[i].discount;
+  total.value = allProducts[i].total;
+  getTotal();
+  category.value = allProducts[i].category;
+  count.style.display = "none";
+  submit.innerHTML = "update";
+  mode = "update";
+  num = i;
+  scrollTo({
+    top: 0,
+  });
+}
 
+/** 8- start search **/
+let searchMode = "title";
+function getSearchMode(id) {
+  if (id == "search-title") {
+    searchMode = "title";
+  } else {
+    searchMode = "category";
+  }
+  search.value = "";
+  search.placeholder = `search by ${searchMode}`;
+  search.focus();
+  showData();
+}
+function searchProducts(keyWord) {
+  let table = "";
+  for (let i = 0; i < allProducts.length; i++) {
+    if (searchMode == "title") {
+      if (allProducts[i].productTitle.includes(keyWord.toLowerCase())) {
+        table += `
+          <tr>
+            <td>${i}</td>
+            <td>${allProducts[i].productTitle}</td>
+            <td>${allProducts[i].productPrice}</td>
+            <td>${allProducts[i].taxes}</td>
+            <td>${allProducts[i].ads}</td>
+            <td>${allProducts[i].discount}</td>
+            <td>${allProducts[i].total}</td>
+            <td>${allProducts[i].count}</td>
+            <td>${allProducts[i].category}</td>
+            <td><button onclick="updateProduct(${i})" id="update">update</button></td>
+            <td><button onclick="deleteProduct(${i})" id="delete">delete</button></td>
+          </tr>
+        `;
+      }
+    } else {
+      if (allProducts[i].category.includes(keyWord.toLowerCase())) {
+        table += `
+          <tr>
+            <td>${i}</td>
+            <td>${allProducts[i].productTitle}</td>
+            <td>${allProducts[i].productPrice}</td>
+            <td>${allProducts[i].taxes}</td>
+            <td>${allProducts[i].ads}</td>
+            <td>${allProducts[i].discount}</td>
+            <td>${allProducts[i].total}</td>
+            <td>${allProducts[i].count}</td>
+            <td>${allProducts[i].category}</td>
+            <td><button onclick="updateProduct(${i})" id="update">update</button></td>
+            <td><button onclick="deleteProduct(${i})" id="delete">delete</button></td>
+          </tr>
+        `;
+      }
+    }
+  }
+  document.getElementById("tbody").innerHTML = table;
+}
+/** end search **/
